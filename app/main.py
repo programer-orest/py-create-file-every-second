33bf62ed147f28335581788d59d5ba3763f54471
Orest Yakymiv <akimivorest@gmail.com>
2025-03-28 20:47:20 +0200

    applied the changes

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         date = datetime.now()
         only_time = date.strftime("%H_%M_%S")
         name_of_file = f"app-{only_time}.log"
-        with open(name_of_file, 'w') as f:
+        with open(name_of_file, "w") as f:
             f.write(date.strftime("%Y-%m-%d %H:%M:%S"))
         sleep(1)
         with open(name_of_file, "r") as f:
