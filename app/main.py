bfcd8a9fcbf4c8a8d9d4c5303d5eeda1afabe114
Orest Yakymiv <akimivorest@gmail.com>
2025-03-28 20:45:15 +0200

    create function with context manager of readind and writing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        date = datetime.now()
+        only_time = date.strftime("%H_%M_%S")
+        name_of_file = f"app-{only_time}.log"
+        with open(name_of_file, 'w') as f:
+            f.write(date.strftime("%Y-%m-%d %H:%M:%S"))
+        sleep(1)
+        with open(name_of_file, "r") as f:
+            print(f.read(), name_of_file)
 
 
 if __name__ == "__main__":
